ba18e42c9e1988a9d4d505bb15668ace5b98daf8
Josh Triplett <josh@joshtriplett.org>
2016-11-23 00:46:58 -0800

    Rewrite to avoid invoking cargo directly on .crate contents
    According to Cargo upstream, crates don't necessarily support running
    cargo on them directly; a .crate file only includes enough to build as a
    dependency or with "cargo install $crate", not as the top-level crate.
    For instance, when building as the top-level crate, cargo expects
    other crates referenced by workspaces to exist; otherwise, cargo ignores
    workspaces.

--- cargo.pm
+++ cargo.pm
@@ -6,8 +6,11 @@ package Debian::Debhelper::Buildsystem::cargo;
 
 use strict;
 use warnings;
-use Debian::Debhelper::Dh_Lib qw(doit error);
+use Cwd;
+use Debian::Debhelper::Dh_Lib;
+use Dpkg::Changelog::Debian;
 use Dpkg::Control::Info;
+use Dpkg::Version;
 use base 'Debian::Debhelper::Buildsystem';
 
 sub DESCRIPTION {
@@ -33,10 +36,22 @@ sub pre_building_step {
     my $this = shift;
     my $step = shift;
 
-    $this->{cargo_home} = $this->get_buildpath("debian/cargo_home");
-    $ENV{'CARGO_HOME'} = $this->{cargo_home};
+    $this->{cargo_home} = Cwd::abs_path($this->get_sourcepath("debian/cargo_home"));
+    $this->{cargo_registry} = Cwd::abs_path($this->get_sourcepath("debian/cargo_registry"));
 
     my $control = Dpkg::Control::Info->new();
+
+    my $source = $control->get_source();
+    my $crate = $source->{'X-Cargo-Crate'};
+    if (!$crate) {
+        $crate = $source->{Source};
+        $crate =~ s/^rust-//;
+    }
+    $this->{crate} = $crate;
+    my $changelog = Dpkg::Changelog::Debian->new(range => { count => 1 });
+    $changelog->load($this->get_sourcepath("debian/changelog"));
+    $this->{version} = Dpkg::Version->new(@{$changelog}[0]->get_version())->version();
+
     my @packages = $control->get_packages();
     $this->{libpkg} = 0;
     $this->{binpkg} = 0;
@@ -57,63 +72,67 @@ sub pre_building_step {
     $this->SUPER::pre_building_step($step);
 }
 
-sub configure {
+sub get_sources {
     my $this=shift;
-    doit("mkdir", "-p", $this->{cargo_home});
-    doit("cp", "/usr/share/cargo/config", $this->{cargo_home});
+    opendir(my $dirhandle, $this->get_sourcedir());
+    my @sources = grep { $_ ne '.' && $_ ne '..' && $_ ne '.git' && $_ ne 'debian' } readdir($dirhandle);
+    closedir($dirhandle);
+    @sources
 }
 
-sub build {
+sub configure {
     my $this=shift;
-    if ($this->{libpkg}) {
-        # Could skip this and copy the files directly in install if "cargo
-        # package --list0" existed.  See
-        # https://github.com/rust-lang/cargo/issues/3306
-        doit("cargo", "package", "--no-verify");
-    }
-    if ($this->{binpkg}) {
-        doit("cargo", "build", "--release", @{$this->{j}});
-    }
 }
 
 sub install {
     my $this=shift;
+    my $crate = $this->{crate} . '-' . $this->{version};
     if ($this->{libpkg}) {
-        my $target = $this->get_sourcepath("debian/" . $this->{libpkg} . "/usr/share/cargo/registry/");
-        my $pkgdir = $this->get_sourcepath("target/package");
-        opendir(my $dh, $pkgdir);
-        my @crates = grep(/\.crate$/, readdir($dh));
-        closedir($dh);
-        if (@crates != 1) {
-            error("Could not find unique .crate file in $pkgdir");
-        }
-        my $crate_name = $crates[0];
-        my $crate = "$pkgdir/$crate_name";
-        $crate_name =~ s/\.crate$//;
+        my $target = $this->get_sourcepath("debian/" . $this->{libpkg} . "/usr/share/cargo/registry/$crate");
+        my @sources = $this->get_sources();
         doit("mkdir", "-p", $target);
-        doit("tar", "-C", $target, "-x", "--anchored", "--no-wildcards-match-slash", "--exclude=*/debian", "-f", $crate);
-        doit("cp", $this->get_sourcepath("debian/cargo-checksum.json"), "$target/$crate_name/.cargo-checksum.json");
+        doit("cp", "-at", $target, @sources);
+        doit("cp", $this->get_sourcepath("debian/cargo-checksum.json"), "$target/.cargo-checksum.json");
     }
     if ($this->{binpkg}) {
+        my $registry = $this->{cargo_registry};
+        doit("mkdir", "-p", $this->{cargo_home}, $registry);
+        opendir(my $dirhandle, '/usr/share/cargo/registry');
+        my @crates = map { "/usr/share/cargo/registry/$_" } grep { $_ ne '.' && $_ ne '..' } readdir($dirhandle);
+        closedir($dirhandle);
+        if (@crates) {
+            doit("ln", "-st", "$registry", @crates);
+        }
+        # Handle the case of building the package with the same version of the
+        # package installed.
+        if (-l "$registry/$crate") {
+            unlink("$registry/$crate");
+        }
+        mkdir("$registry/$crate");
+        my @sources = $this->get_sources();
+        doit("cp", "-at", "$registry/$crate", @sources);
+        doit("cp", $this->get_sourcepath("debian/cargo-checksum.json"), "$registry/$crate/.cargo-checksum.json");
+
+        open(CONFIG, ">" . $this->{cargo_home} . "/config");
+        print(CONFIG qq{
+[source.crates-io]
+replace-with = "dh-cargo-registry"
+
+[source.dh-cargo-registry]
+directory = "$registry"
+});
+        close(CONFIG);
+        $ENV{'CARGO_HOME'} = $this->{cargo_home};
+
         my $target = $this->get_sourcepath("debian/" . $this->{binpkg} . "/usr");
-        doit("cargo", "install", "--root", $target);
+        doit("cargo", "install", $this->{crate}, "--vers", $this->{version}, "--root", $target, @{$this->{j}});
         doit("rm", "$target/.crates.toml");
     }
 }
 
-sub test {
-    my $this=shift;
-    doit("cargo", "test", "--release", @{$this->{j}});
-}
-
 sub clean {
     my $this=shift;
-    doit("cargo", "clean");
-    doit("rm", "-rf", $this->{cargo_home});
-    # For now, always delete Cargo.lock, since the upstream crates never
-    # include it. This may need to change if future crates start including it.
-    # See https://github.com/rust-lang/cargo/issues/2263
-    doit("rm", "-f", $this->get_sourcepath('Cargo.lock'));
+    doit("rm", "-rf", $this->{cargo_home}, $this->{cargo_registry});
 }
 
 1
